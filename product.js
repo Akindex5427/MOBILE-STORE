0745037fff9af7eb1e1fd84394192e2cdf8c7a03
Akindex5427 <56794552+Akindex5427@users.noreply.github.com>
2021-05-15 01:34:02 +0100

    Add files via upload

---
+++ product.js
@@ -0,0 +1,105 @@
+const getProducts = async () => {
+    const res = await fetch("products.json");
+    const data = await res.json();
+    const products = data.products;
+    return products;
+};
+
+// Display Product
+const displayProducts = (products, center) => {
+    let display = products.map(({title,image,price}) => `<div class="product">
+                      <div class="product-header">
+                          <img src=${image} alt="product">
+                      </div>
+                      <div class="product-footer">
+                          <h5>${title}</h5>
+                          <div class="rating">
+                              <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                             <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                          </div>
+                          <div class="product-price">
+                              <h6>$${price}</h6>
+                          </div>
+                          <ul>
+                              <li>
+                                  <a href="#">
+                                      <i class="far fa-eye"></i>
+                                  </a>
+                              </li>
+                            <li>
+                                <a href="#">
+                                    <i class="far fa-heart"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fas fa-sync"></i>
+                                </a>
+                            </li>
+                          </ul>
+                      </div>
+                  </div>`
+        );
+
+        display = display.join("");
+        center.innerHTML = display;
+};
+
+//  Filtering
+
+const catContainer = document.querySelector(".sort-category");
+const filterBtns = [...document.querySelectorAll(".filter-btn")];
+
+
+if(catContainer) {
+     catContainer.addEventListener("click", async e => {
+    const target = e.target.closest(".section-title");
+    if (!target) return;
+
+    const id = target.dataset.id;
+    const products = await getProducts();
+    
+    if (id) {
+        filterBtns.forEach(btn => {
+        btn.classList.remove("active");
+       });
+       target.classList.add("active");
+       const menuCat = products.filter(product => product.category === id);
+       productCenter.classList.add("animate__animated", "animate__backInUp");
+       setTimeout(() => {
+           productCenter.classList.remove(
+               "animate__animated", 
+               "animate__backInUp"
+               );
+       }, 1000);
+
+       displayProducts(menuCat, productCenter);
+      } 
+    });
+
+  }
+
+    const productCenter = document.querySelector(".product-center");
+    const shopCenter = document.querySelector(".shop-center");
+    const latestCenter = document.querySelector(".latest-center");
+    const recentCenter = document.querySelector(".recent-center");
+
+    const filterArray = async type => {
+        const products = await getProducts();
+        return products.filter(product => product.category === type);
+    };
+
+    window.addEventListener("DOMContentLoaded", async () => {
+        const products = await getProducts();
+        const defaultProducts = await filterArray("trend");
+        const latestProducts = await filterArray("latest");
+        const recentProducts = await filterArray("recent");
+        displayProducts(defaultProducts, productCenter);
+        displayProducts(latestProducts, latestCenter);
+        displayProducts(recentProducts, recentCenter);
+        displayProducts(products, shopCenter);
+    });
+ 
